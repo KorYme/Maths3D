55f875855109d52444295d5970e4a0c490757c6c
KorYme <92892240+KorYme@users.noreply.github.com>
2024-09-18 11:34:24 +0200

    [PROG] : Exo 1 et 2

--- Maths3D/Maths3DClass/MatrixInt.cs
+++ Maths3D/Maths3DClass/MatrixInt.cs
@@ -4,37 +4,55 @@ public struct MatrixInt
 {
     private int[,] m_matrix;
     
-    public int RowsCount => m_matrix.GetLength(0);
-    public int ColumnsCount => m_matrix.GetLength(1);
+    public int NbLines => m_matrix.GetLength(0);
+    public int NbColumns => m_matrix.GetLength(1);
 
     public MatrixInt(int[,] matrix)
     {
         m_matrix = matrix;
     }
 
+    public MatrixInt(int NbLines, int NbColumns)
+    {
+        m_matrix = new int[NbLines, NbColumns];
+    }
+
+    public MatrixInt(MatrixInt copy)
+    {
+        m_matrix = copy.ToArray2D();
+    }
+
     public int this[int rowIndex, int columnIndex]
     {
         get
         {
-            if (rowIndex < 0 || rowIndex >= RowsCount || columnIndex < 0 || columnIndex >= ColumnsCount)
+            if (rowIndex < 0 || rowIndex >= NbLines || columnIndex < 0 || columnIndex >= NbColumns)
             {
                 throw new IndexOutOfRangeException();
             }
             return m_matrix[rowIndex, columnIndex];
-        } 
+        }
+        set
+        {
+            if (rowIndex < 0 || rowIndex >= NbLines || columnIndex < 0 || columnIndex >= NbColumns)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            m_matrix[rowIndex, columnIndex] = value;
+        }
     }
     
     public int[,] ToArray2D()
     {
-        return m_matrix;
+        return (int[,])m_matrix.Clone();
     }
 
     public MatrixInt Transpose()
     {
-        int[,] tmp = new int[ColumnsCount, RowsCount];
-        for (int rowIndex = 0; rowIndex < RowsCount; rowIndex++)
+        int[,] tmp = new int[NbColumns, NbLines];
+        for (int rowIndex = 0; rowIndex < NbLines; rowIndex++)
         {
-            for (int columnIndex = 0; columnIndex < ColumnsCount; columnIndex++)
+            for (int columnIndex = 0; columnIndex < NbColumns; columnIndex++)
             {
                 int tmpValue = this[rowIndex, columnIndex];
                 tmp[columnIndex, rowIndex] = tmpValue;
