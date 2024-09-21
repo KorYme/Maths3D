0c720321d12ff9af1da93377ef1380bdb93b3726
KorYme <92892240+KorYme@users.noreply.github.com>
2024-09-21 18:59:14 +0200

    [PROG] : Exercice 18 Transformation Local Rotation

--- Maths3D/Maths3DClass/MyJobClasses/Transform.cs
+++ Maths3D/Maths3DClass/MyJobClasses/Transform.cs
@@ -4,6 +4,7 @@ public class Transform
 {
     #region PROPERTIES
     public Vector3 LocalPosition { get; set; }
+    public Vector3 LocalRotation { get; set; }
 
     public Matrix<float> LocalTranslationMatrix
     {
@@ -14,6 +15,60 @@ public class Transform
             { 0f, 0f, 1f, LocalPosition.z },
             { 0f, 0f, 0f, 1f },
         });
+    }
+
+    // TO DO²
+    public Matrix<float> LocalRotationXMatrix
+    {
+        get
+        {
+            float cosAngle = MathsUtilities.CosWithDeg(LocalRotation.x);
+            float sinAngle = MathsUtilities.SinWithDeg(LocalRotation.x);
+            return new Matrix<float>(new float[4, 4]
+            {
+                { 1f, 0f, 0f, 0f },
+                { 0f, cosAngle, -sinAngle, 0f },
+                { 0f, sinAngle, cosAngle, 0f },
+                { 0f, 0f, 0f, 1f },
+            });
+        } 
+    }
+    
+    public Matrix<float> LocalRotationYMatrix
+    {
+        get
+        {
+            float cosAngle = MathsUtilities.CosWithDeg(LocalRotation.y);
+            float sinAngle = MathsUtilities.SinWithDeg(LocalRotation.y);
+            return new Matrix<float>(new float[4, 4]
+            {
+                { cosAngle, 0f, sinAngle, 0f },
+                { 0f, 1f, 0f, 0f },
+                { -sinAngle, 0f, cosAngle, 0f },
+                { 0f, 0f, 0f, 1f },
+            });
+        } 
+    }
+    
+    public Matrix<float> LocalRotationZMatrix
+    {
+        get
+        {
+            float cosAngle = MathsUtilities.CosWithDeg(LocalRotation.z);
+            float sinAngle = MathsUtilities.SinWithDeg(LocalRotation.z);
+            return new Matrix<float>(new float[4, 4]
+            {
+                { cosAngle, -sinAngle, 0f, 0f },
+                { sinAngle, cosAngle, 0f, 0f },
+                { 0f, 0f, 1f, 0f },
+                { 0f, 0f, 0f, 1f },
+            });
+        } 
+    }
+    
+    public Matrix<float> LocalRotationMatrix
+    {
+        get => LocalRotationYMatrix * LocalRotationXMatrix * LocalRotationZMatrix;
     }
     #endregion
     
