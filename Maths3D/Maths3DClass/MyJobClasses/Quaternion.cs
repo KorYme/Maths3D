349bf3b1e1feee7b8ded297bbe881f55dd4f60d7
KorYme <92892240+KorYme@users.noreply.github.com>
2024-09-22 18:34:28 +0200

    [PROG] : Exercise 26 Quaternions Point Rotation

--- Maths3D/Maths3DClass/MyJobClasses/Quaternion.cs
+++ Maths3D/Maths3DClass/MyJobClasses/Quaternion.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace Maths_Matrices.Tests;
 
 public struct Quaternion
@@ -7,10 +9,20 @@ public struct Quaternion
     public float y { get; set; }
     public float z { get; set; }
     public float w { get; set; }
+
+    public Matrix<float> rotationMatrix
+    {
+        get => new Matrix<float>(new float[3,3]
+        {
+            { 1-2*y*y-2*z*z, 2*x*y-2*w*z, 2*x*z+2*w*y },
+            { 2*x*y+2*w*z, 1-2*x*x-2*z*z, 2*y*z-2*w*x },
+            { 2*x*z-2*w*y, 2*y*z+2*w*x, 1-2*x*x-2*y*y },
+        });
+    }
     #endregion
 
     #region STATIC PROPERTIES
-    public static Quaternion Identity => new(0f, 0f, 0f, 1f);
+    public static Quaternion Identity => new Quaternion(0f, 0f, 0f, 1f);
     #endregion
 
     #region CONSTRUCTORS
@@ -40,6 +52,11 @@ public struct Quaternion
             q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z * q2.x,
             q1.w * q2.z + q1.x * q2.y - q1.y * q2.x + q1.z * q2.w,
             q1.w * q2.w + q1.x * q2.x + q1.y * q2.y + q1.z * q2.z);
+    }
+
+    public static Vector3 operator *(Quaternion q, Vector3 point)
+    {
+        return (Vector3)(q.rotationMatrix * point.GetMatrix());
     }
     #endregion
 }
