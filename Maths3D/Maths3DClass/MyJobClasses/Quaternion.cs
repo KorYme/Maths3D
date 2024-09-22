15cf6b8d3d25a39d327ff2022cdaaacdbb9a00b1
KorYme <92892240+KorYme@users.noreply.github.com>
2024-09-22 16:54:40 +0200

    [PROG] : Exercise 25 Quaternion Multiplication

--- Maths3D/Maths3DClass/MyJobClasses/Quaternion.cs
+++ Maths3D/Maths3DClass/MyJobClasses/Quaternion.cs
@@ -29,9 +29,17 @@ public struct Quaternion
         float sin = MathsUtilities.SinWithDeg(angle / 2f);
         float cos = MathsUtilities.CosWithDeg(angle / 2f);
         Vector3 axisNormalized = axis.Normalized();
-        Quaternion result = new Quaternion(axisNormalized.x * sin, axisNormalized.y * sin, 
-            axisNormalized.z * sin, cos);
-        return result;
+        return new Quaternion(axisNormalized.x * sin, axisNormalized.y * sin, axisNormalized.z * sin, cos);
+    }
+    #endregion
+
+    #region OPERATOR OVERRIDES
+    public static Quaternion operator *(Quaternion q1, Quaternion q2)
+    {
+        return new Quaternion(q1.w * q2.x + q1.x * q2.w + q1.y * q2.z - q1.z * q2.y,
+            q1.w * q2.y - q1.x * q2.z + q1.y * q2.w + q1.z * q2.x,
+            q1.w * q2.z + q1.x * q2.y - q1.y * q2.x + q1.z * q2.w,
+            q1.w * q2.w + q1.x * q2.x + q1.y * q2.y + q1.z * q2.z);
     }
     #endregion
 }
